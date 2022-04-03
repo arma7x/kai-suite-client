4b322ea3a670f6e71799a5679d82e00607923495
arma7x <arma7x@live.com>
2022-04-03 16:39:39 +0800

    Search phonebook by pressing button Call

--- src/routes/Contacts.svelte
+++ src/routes/Contacts.svelte
@@ -3,12 +3,19 @@
   import { createKaiNavigator } from '../utils/navigation';
   import { onMount, onDestroy } from 'svelte';
   import { getDeviceContacts } from '../synchub/utils';
-  import { ListView } from '../components';
+  import { ListView, TextInputDialog, LoadingBar } from '../components';
 
   export let location: any;
   export let navigate: any;
   export let getAppProp: Function;
 
+  const chunkSize = 50;
+
+  let loadingBar: LoadingBar;
+  let contactSearchDialog: TextInputDialog;
+
+  let contactDb: Array<any> = [];
+  let searchInput: string = '';
   let name: string = 'Phonebook';
   let pageCursor: int = -1;
   let pages: Array<any> = [];
@@ -25,6 +32,10 @@
       nextPage(pageCursor)
     },
     enterListener: function(evt) {
+      const navClasses = document.getElementsByClassName('contactsNav');
+      if (navClasses[this.verticalNavIndex] != null) {
+        navClasses[this.verticalNavIndex].click();
+      }
       console.log('enterListener', name);
     },
     backspaceListener: function(evt) {
@@ -42,25 +53,48 @@
     appBar.setTitleText(name);
     softwareKey.setText({ left: `Prev`, center: `SELECT`, right: `Next` });
     navInstance.attachListener();
+    showLoadingBar();
     getDeviceContacts()
     .then((contacts) => {
-      const chunkSize = 50;
-      for (let i = 0; i < contacts.length; i += chunkSize) {
-        const chunk = contacts.slice(i, i + chunkSize);
+      contactDb = [...contacts]
+      for (let i = 0; i < contactDb.length; i += chunkSize) {
+        const chunk = contactDb.slice(i, i + chunkSize);
         pages.push(chunk);
       }
-      console.log(pages)
       nextPage(pageCursor)
+      //console.log(pages)
     })
     .catch(err => {
       console.warn(err)
+    })
+    .finally(() => {
+      if (loadingBar != null) {
+        loadingBar.$destroy();
+      }
     });
+    document.addEventListener('keydown', callButtonHandler);
   });
 
   onDestroy(() => {
     console.log('onDestroy', name);
+    document.removeEventListener('keydown', callButtonHandler);
     navInstance.detachListener();
   });
+
+  function showLoadingBar() {
+    loadingBar = new LoadingBar({
+      target: document.body,
+      props: {
+        onOpened: () => {
+          navInstance.detachListener();
+        },
+        onClosed: () => {
+          navInstance.attachListener();
+          loadingBar = null;
+        }
+      }
+    });
+  }
 
   function prevPage(p) {
     if (pages[p - 1] != null) {
@@ -94,6 +128,79 @@
 
   function select(user) {
     console.log(user)
+  }
+
+  function callButtonHandler(evt) {
+    if (evt.key === 'Call') {
+      console.log('callButtonHandler', evt.key)
+      openContactSearchDialog()
+    }
+  }
+
+  function openContactSearchDialog() {
+    contactSearchDialog = new TextInputDialog({
+      target: document.body,
+      props: {
+        title: 'Search Contacs',
+        softKeyLeftText: 'Cancel',
+        softKeyCenterText: 'ok',
+        value: searchInput,
+        placeholder: 'Enter search keyword',
+        type: 'text',
+        onSoftkeyLeft: (evt, value) => {
+          console.log('onSoftkeyLeft', value);
+          contactSearchDialog.$destroy();
+        },
+        onSoftkeyRight: (evt, value) => {
+          console.log('onSoftkeyRight', value);
+        },
+        onEnter: (evt, value) => {
+          console.log('onEnter', value);
+          searchContacts(value.trim());
+          contactSearchDialog.$destroy();
+        },
+        onBackspace: (evt, value) => {
+          console.log('onBackspace', value);
+          evt.stopPropagation();
+        },
+        onOpened: () => {
+          navInstance.detachListener();
+        },
+        onClosed: (value) => {
+          console.log('onClosed', value)
+          navInstance.attachListener();
+          contactSearchDialog = null;
+        }
+      }
+    });
+  }
+
+  function searchContacts(keyword) {
+    searchInput = keyword ? keyword : '';
+    if (searchInput) {
+      pages = []
+      console.log('search searchContacts', searchInput.length)
+      const searchResult = contactDb.filter((user) => {
+        const cmp = user.name[0].toLocaleLowerCase().indexOf(searchInput.toLocaleLowerCase());
+        return cmp > -1;
+      })
+      console.log('searchResult', searchResult.length)
+      pageCursor = -1;
+      for (let i = 0; i < searchResult.length; i += chunkSize) {
+        const chunk = searchResult.slice(i, i + chunkSize);
+        pages.push(chunk);
+      }
+      nextPage(pageCursor)
+    } else {
+      pages = []
+      console.log('reset searchContacts', searchInput.length)
+      pageCursor = -1;
+      for (let i = 0; i < contactDb.length; i += chunkSize) {
+        const chunk = contactDb.slice(i, i + chunkSize);
+        pages.push(chunk);
+      }
+      nextPage(pageCursor)
+    }
   }
 
 </script>
